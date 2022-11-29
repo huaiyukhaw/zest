985d941c5cb7550a4298b5986844f829ed107840
Huaiyu Khaw <huaiyukhaw@gmail.com>
2022-11-30 00:23:27 +0800

    fix missing userEmail in seed

--- app/components/form/index.ts
+++ app/components/form/index.ts
@@ -3,5 +3,4 @@ export * from "./FormSelect";
 export * from "./FormTextArea";
 export * from "./SubmitButton";
 export * from "./YearSelect";
-export * from "./SearchField";
 export * from "./FormImageInput";
