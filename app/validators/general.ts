3f2a648fb5debdb737b08a7f2b61c374dd39c861
Huaiyu Khaw <huaiyukhaw@gmail.com>
2023-02-05 00:43:14 +0800

    remove app|posts|login|api|join|login|logout|healthcheck|index options from username

--- app/validators/general.ts
+++ app/validators/general.ts
@@ -1,28 +1,10 @@
 import { withZod } from "@remix-validated-form/with-zod";
 import { z } from "zod";
 import { isUsernameAvailable } from "~/models/profile.server";
+import { usernameSchema } from "./profile";
 
 export const generalSchema = z.object({
-  username: z
-    .string({
-      invalid_type_error:
-        "Your username can only contain letters, numbers and '_'",
-      required_error: "Username is required",
-    })
-    .trim()
-    .min(4, {
-      message: "Your username must be longer than 3 characters.",
-    })
-    .max(15, {
-      message: "Your username must be shorter than 15 characters.",
-    })
-    .regex(/[a-z_]/, {
-      message: "Include a non-number character",
-    })
-    .regex(/^[a-z0-9_]*$/, {
-      message:
-        "Your username can only contain lowercase letters, numbers and '_'",
-    }),
+  username: usernameSchema,
   currentUsername: z.string(),
   displayName: z
     .string({
--- app/validators/profile.ts
+++ app/validators/profile.ts
@@ -2,30 +2,40 @@ import { withZod } from "@remix-validated-form/with-zod";
 import { z } from "zod";
 import { isUsernameAvailable } from "~/models/profile.server";
 
+export const usernameSchema = z
+  .string({
+    invalid_type_error:
+      "Your username can only contain letters, numbers and '_'",
+    required_error: "Username is required",
+  })
+  .trim()
+  .min(4, {
+    message: "Your username must be longer than 3 characters.",
+  })
+  .max(15, {
+    message: "Your username must be shorter than 15 characters.",
+  })
+  .regex(/[a-z_]/, {
+    message: "Include a non-number character.",
+  })
+  .regex(/^[a-z0-9_]*$/, {
+    message: "Your username can only contain letters, numbers and '_'",
+  })
+  .regex(
+    /^(?!(?:app|posts|login|api|join|login|logout|healthcheck|index)$).*$/,
+    {
+      message: "That username is not available.",
+    }
+  );
+
 export const profileSchema = z.object({
-  username: z
-    .string({
-      invalid_type_error:
-        "Your username can only contain letters, numbers and '_'",
-      required_error: "Username is required",
-    })
-    .min(4, {
-      message: "Your username must be longer than 3 characters.",
-    })
-    .max(15, {
-      message: "Your username must be shorter than 15 characters.",
-    })
-    .regex(/[a-z_]/, {
-      message: "Include a non-number character",
-    })
-    .regex(/^[a-z0-9_]*$/, {
-      message: "Your username can only contain letters, numbers and '_'",
-    }),
+  username: usernameSchema,
   displayName: z
     .string({
       invalid_type_error: "Your display name can only be text.",
       required_error: "Display name is required.",
     })
+    .trim()
     .min(1, {
       message: "Display name is required.",
     })
