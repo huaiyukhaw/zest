de52f2ed0459af216d95a94a59c75247699ab02c
Huaiyu Khaw <huaiyukhaw@gmail.com>
2023-03-13 15:54:01 +0800

    add back to all tags link

--- app/routes/tag/$tagId.tsx
+++ app/routes/tag/$tagId.tsx
@@ -37,11 +37,13 @@ const PostsIndexPage = () => {
     return (
         <div className="max-w-screen-md mx-auto mt-4 mb-20 sm:mt-20 px-4">
             <div className="flex items-baseline gap-4 my-6">
-                <div className="rounded-full p-2 bg-gray-300/50 dark:bg-gray-600/50 text-gray-700 dark:text-gray-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 -scale-x-100">
-                        <path fillRule="evenodd" d="M5.25 2.25a3 3 0 00-3 3v4.318a3 3 0 00.879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 005.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 00-2.122-.879H5.25zM6.375 7.5a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" clipRule="evenodd" />
-                    </svg>
-                </div>
+                <Link to="../tag">
+                    <div className="rounded-full p-2 bg-gray-300/50 dark:bg-gray-600/50 text-gray-700 dark:text-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 -scale-x-100">
+                            <path fillRule="evenodd" d="M5.25 2.25a3 3 0 00-3 3v4.318a3 3 0 00.879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 005.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 00-2.122-.879H5.25zM6.375 7.5a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" clipRule="evenodd" />
+                        </svg>
+                    </div>
+                </Link>
                 <h1 className="text-5xl font-extrabold text-gray-700 dark:text-gray-200">
                     {tagId}
                 </h1>
